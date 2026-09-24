71f4bbb21550042b11549d3af3c41dd83d526fc2
Emma DA SILVA <emmambamarah@gmail.com>
2026-09-24 18:24:18 +0100

    Corriger l'orientation des enseignes 3D

--- src/components/experience/NovotelExterior.tsx
+++ src/components/experience/NovotelExterior.tsx
@@ -178,7 +178,7 @@ function HotelBlock() {
 
       <Text
         position={[0, 0.68, -0.86]}
-        fontSize={0.17}
+        rotation={[0, Math.PI, 0]}\n        fontSize={0.17}
         letterSpacing={0.1}
         color="#363735"
         anchorX="center"
